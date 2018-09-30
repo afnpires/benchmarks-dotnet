87b2647813bd5c4446f9fa3ce2046b947a38c596
Alexandre Pires <afnpires@outlook.com>
2018-09-30 12:10:34 +0100

    chore: Setup project

---
+++ afnpires.Benchmarks.DotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace afnpires.Benchmarks.DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
